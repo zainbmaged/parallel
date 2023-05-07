234f009c03e9dd483b52cafc03fa5d7712dbd513
Tasneem Mohamed <92854521+Tasneem849@users.noreply.github.com>
2023-05-07 03:57:46 +0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-CATEGORY_CHOICES=(
-    ('SL','silk'),
+STATE_CHOICES = (
+    ('NC', 'New Cairo'),
+    ('AP', 'Abdou Pasha'),
+)
+
+CATEGORY_CHOICES = (
+    ('SL','Silk'),
     ('CO','Cotton'),
     ('CH','Chiffon'),
 )
@@ -12,10 +18,22 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     selling_price = models.FloatField()
     discounted_price = models.FloatField()
-    description = models.TimeField()
+    description = models.TextField()
     composition = models.TextField(default='')
     prodapp = models.TextField(default='')
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
-    product_image= models.ImageField(upload_to = 'product')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=20)
+    product_image = models.ImageField(upload_to = 'product')
     def __str__(self):
         return self.title
+    
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
